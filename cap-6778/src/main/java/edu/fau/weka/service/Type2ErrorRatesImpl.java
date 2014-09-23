a13d2c1dd905330e7e81bb84bc7c5a1d40c57c96
john <jhancock1975@gmail.com>
2014-09-23 13:10:14 -0400

    take out 30.0 from cost ratio

--- cap-6778/src/main/java/edu/fau/weka/service/Type2ErrorRatesImpl.java
+++ cap-6778/src/main/java/edu/fau/weka/service/Type2ErrorRatesImpl.java
@@ -9,7 +9,7 @@ public class Type2ErrorRatesImpl implements Type2ErrorRatesService {
 
 	public List<Double> getType2ErrorRates() {
 		return Arrays.asList(new Double[] {0.125, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 20.0,
-				21.0, 21.5, 22.0, 22.5, 23.0, 24.0, 28.0, 30.0, 31.0, 32.0, 34.0, 36.0, 40.0, 64.0, 128.0});
+				21.0, 21.5, 22.0, 22.5, 23.0, 24.0, 28.0, 31.0, 32.0, 34.0, 36.0, 40.0, 64.0, 128.0});
 	}
 
 }
