54a82590ca4f65c92b81c99f035bbd8878a18e39
john <jhancock1975@gmail.com>
2014-10-07 16:55:52 -0400

    delete unnecessary interface

--- cap-6778/src/main/java/edu/fau/weka/model/ClassifierResults.java
+++ cap-6778/src/main/java/edu/fau/weka/model/ClassifierResults.java
@@ -2,31 +2,126 @@ package edu.fau.weka.model;
 
 import java.util.Date;
 
-import weka.classifiers.Evaluation;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
-public interface ClassifierResults {
-	public String getClassifierClassName();
-	public void setClassifierClassName(String className);
-	public String getFilterClassName();
-	public void setFilterClassName(String className);
-	public int getFeatureSetSize();
-	public void setFeatureSetSize(int size);
-	public Evaluation getEvaluation();
-	public Evaluation setEvaluation(Evaluation evaluation);
-	public void setFeatureSet(String featureSet);
-	public String getFeatureSet();
-	public Double getpAuc();
-	public void setpAuc(Double pAuc);
-	public Double getnAuc();
-	public void setnAuc(Double nAuc);
-	public Double getwAuc();
-	public void setwAuc(Double wAuc);
-	public Double getFpr();
-	public void setFpr(Double fpr);
-	public Double getFnr();
-	public void setFnr(Double fnr);
-	public void setExperimentStartTime(Date startTime);
-	public Date getExperimentStartTime();
-	public Integer getClassifierResultId();
+import org.springframework.stereotype.Component;
+
+import weka.classifiers.Evaluation;
+@Component
+@Entity(name="ClassifierResults")
+public class ClassifierResults {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer classifierResultId;
+	@Transient
+	private Evaluation evaluation;
+	
+	private String classifierClassName;
+	
+	private String filterClassName;
+	
+	private Integer featureSetSize;
+	
+	private Double fpr;
+	 
+	private Double fnr;
+	
+	private Double pAuc;
+	
+	private Double nAuc;
+	
+	private String featureSet;
+	@Column(name="experimentStartTime")
+	private Date startTime;
+	
+	public String getFeatureSet() {
+		return featureSet;
+	}
+	public void setFeatureSet(String featureSet) {
+		this.featureSet = featureSet;
+	}
+	public Double getpAuc() {
+		return pAuc;
+	}
+	public void setpAuc(Double pAuc) {
+		this.pAuc = pAuc;
+	}
+	public Double getnAuc() {
+		return nAuc;
+	}
+	public void setnAuc(Double nAuc) {
+		this.nAuc = nAuc;
+	}
+	public Double getwAuc() {
+		return wAuc;
+	}
+	public void setwAuc(Double wAuc) {
+		this.wAuc = wAuc;
+	}
+	private Double wAuc;
+	
+	private String instances;
+	
+	public Double getFpr() {
+		return fpr;
+	}
+	public void setFpr(Double fpr) {
+		this.fpr = fpr;
+	}
+	public Double getFnr() {
+		return fnr;
+	}
+	public void setFnr(Double fnr) {
+		this.fnr = fnr;
+	}
+	public String getInstances() {
+		return instances;
+	}
+	public void setInstances(String instances) {
+		this.instances = instances;
+	}
+	public void setFeatureSetSize(Integer featureSetSize) {
+		this.featureSetSize = featureSetSize;
+	}
+	public Evaluation getEvaluation() {
+		return evaluation;
+	}
+	public Evaluation setEvaluation(Evaluation evaluation) {
+		this.evaluation = evaluation;
+		return evaluation;
+	}
+	public String getClassifierClassName() {
+		return classifierClassName;
+	}
+	public void setClassifierClassName(String classifierClassName) {
+		this.classifierClassName = classifierClassName;
+	}
+	public String getFilterClassName() {
+		return filterClassName;
+	}
+	public void setFilterClassName(String filterClassName) {
+		this.filterClassName = filterClassName;
+	}
+	public int getFeatureSetSize() {
+		return featureSetSize;
+	}
+	public void setFeatureSetSize(int featureSetSize) {
+		this.featureSetSize = featureSetSize;
+	}
+	public void setExperimentStartTime(Date startTime) {
+		this.startTime = startTime;
+		
+	}
+	public Date getExperimentStartTime() {
+		return startTime;
+	}
+	public Integer getClassifierResultId() {
+		return this.classifierResultId;
+	}
 	
 }
